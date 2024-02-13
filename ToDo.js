3b9d96190688e6398201b63415db93b5e77bf238
Sharmitha Thangavel <subashchandra016@gmail.com>
2024-02-13 23:04:10 +0530

    second

--- ToDo.js
+++ ToDo.js
@@ -4,15 +4,18 @@ import './ToDo.css'
 const ToDo = () => {
   return (
     <div className='whole' >
-     <p className='heading'>Do it Now</p> 
+     <div className='heading'>Do it Now</div> 
      <div className='first'>
       <input className='input' value=" What's the task?"></input>
       <button className='button'>Add</button>
       </div>
-      <p className='task1'>Buy Groceries</p>
-      <p className='task2'>Write a Blog post</p>
-      <p className='task3'>Complete Assignments</p>
-      <p className='task4'>Go for a walk</p>
+      <div className='bottom'>
+      <div className='task1'>Buy Groceries</div>
+      <div className='task2'>Write a Blog post</div>
+      <div className='task3'>Complete Assignments</div>
+      <div className='task4'>Go for a walk</div>
+      </div>
+      
     </div>
   )
 }
