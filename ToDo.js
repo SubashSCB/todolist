378e359d83841b1cfc81cb0cea3c55c0363ab937
Sharmitha Thangavel <subashchandra016@gmail.com>
2024-02-15 01:33:09 +0530

    third commit

--- ToDo.js
+++ ToDo.js
@@ -1,31 +1,26 @@
-import React, { useRef } from 'react'
+import React, { useState } from 'react'
 import './ToDo.css'
 
 const ToDo = () => {
-  const data=useRef();
-  const handleclick=()=>{
-    console.log(data.current.value,"initial value")
-    localStorage.setItem("inputvalue",data.current.value)
-  }
-  console.log(localStorage.getItem("inputvalue"),"****")
+  const[inputValue,setInputValue]=useState("");
+  const toDoArray=["Buy Groceries","Write a Blog post","Complete Assignments","Go for a walk"]
   return (
-    // <div className='whole' >
-    //  <div className='heading'>Do it Now</div> 
-    //  <div className='first'>
-    //   <input className='input' placeholder=" What's the task?"></input>
-    //   <div className='button'>Add</div>
-    //   </div>
-    //   <div className='bottom'>
-    //   <div className='task1'>Buy Groceries</div>
-    //   <div className='task2'>Write a Blog post</div>
-    //   <div className='task3'>Complete Assignments</div>
-    //   <div className='task4'>Go for a walk</div>
-    //   </div>
+    <div className='whole' >
+     <div className='heading'>Do it Now</div> 
+     <div className='first'>
+      <input className='input' placeholder=" What's the task?" value={inputValue} onChange={(text)=>setInputValue(text.target.value)}/>
+      <div className='button' >Add</div>
+      </div>
+      <div className='bottom'>
+        {toDoArray.map((x,i)=>(
+          <div className='task1'>{i+1} {x}</div>
+        ))}
+      {/* <div className='task1'>Buy Groceries</div>
+      <div className='task2'>Write a Blog post</div>
+      <div className='task3'>Complete Assignments</div>
+      <div className='task4'>Go for a walk</div> */}
+      </div>
       
-    // </div>
-    <div>
-      <input ref={data}></input>
-      <button onClick={handleclick}>Add</button>
     </div>
   )
 }
